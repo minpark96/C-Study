1454f91ceaad6b34b00be58f7303fb9f641b8265
minpark96 <hmpark3355@gmail.com>
2023-05-11 03:25:45 +0900

    big

--- ConsoleApplication1/20230510.c
+++ ConsoleApplication1/20230510.c
@@ -132,13 +132,13 @@
 //	int i = 0, j = 0;
 //	while (currentNum < row * col + 1) // 배열을 전부 채웠으면 종료 
 //	{
+//		matrix[i][j] = currentNum;
+//		currentNum++;
+//
 //		switch (dir)
 //		{
 //		case 1: // 오른쪽
-//			matrix[i][j] = currentNum;
-//			currentNum++;
-//
-//			if (j == col - 1 || matrix[i][j + 1] != 0) // 최대 크기에 도달 했거나(최외곽), 0이 아닌 수를 만났을 때 방향 변경(내부)
+//			if (j == col - 1 || matrix[i][j + 1] != 0) // 열이 col - 1에 도달 했거나, 0이 아닌 수를 만났을 때 방향 변경
 //			{
 //				dir++; // 시계 방향 전환 오른쪽(1) -> 아래(2)
 //				i++; // 아래로 한칸 이동
@@ -148,11 +148,8 @@
 //
 //			break;
 //		case 2: // 아래
-//			matrix[i][j] = currentNum;
-//			currentNum++;
-//
-//			if ((i == row - 1 && j == col - 1) || matrix[i + 1][j] != 0) // 최대 크기에 도달 했거나(최외곽), 0이 아닌 수를 만났을 때 방향 변경(내부)
-//			{
+//			if (i == row - 1 || matrix[i + 1][j] != 0) // 행이 row - 1에 도달 했거나, 0이 아닌 수를 만났을 때 방향 변경
+//			{ // 
 //				dir++; // 시계 방향 전환 아래(2) -> 왼쪽(3)
 //				j--; // 왼쪽 한칸 이동
 //			}
@@ -160,11 +157,8 @@
 //				i++; // 아래 한칸 이동
 //
 //			break;
-//		case 3: // 왼쪽
-//			matrix[i][j] = currentNum;
-//			currentNum++;
-//
-//			if (j == 0 || matrix[i][j - 1] != 0) // 최대 크기에 도달 했거나(최외곽), 0이 아닌 수를 만났을 때 방향 변경(내부)
+//		case 3: // 왼쪽 
+//			if (j == 0 || matrix[i][j - 1] != 0) // 열이 0에 도달 했거나, 0이 아닌 수를 만났을 때 방향 변경
 //			{
 //				dir++; // 시계 방향 전환 왼쪽(3) -> 위(4)
 //				i--; // 위로 한칸 이동
@@ -174,10 +168,7 @@
 //
 //			break;
 //		case 4: // 위
-//			matrix[i][j] = currentNum;
-//			currentNum++;
-//
-//			if (matrix[i - 1][j] != 0) // 0이 아닌 수를 만났을 때 방향 변경(내부)
+//			if (matrix[i - 1][j] != 0) // 0이 아닌 수를 만났을 때 방향 변경
 //			{
 //				dir = 1; // 시계 방향 전환 위(4) -> 오른쪽(1)
 //				j++; // 오른쪽 한칸 이동
@@ -204,6 +195,87 @@
 //
 //	return 0;
 //}
+
+int main()
+{
+	int row = 0, col = 0;
+	int length; // 행과 열의 길이
+	int distance; // 이동 거리
+	int count = 1;
+	int dir = 1; // 1-오른쪽, 2-아래, 3-왼쪽, 4-위
+	int matrix[10][10] = { 0 };
+
+	printf("정사각 행렬의 길이를 입력하세요 : ");
+	scanf("%d", &length);
+
+	distance = length;
+	// 규칙 : 오 -> 아, 왼 -> 위로 바뀔 때 이동 거리가 1씩 줄어든다.
+	while (count < length * length + 1) // 배열을 전부 채웠으면 종료 
+	{
+		for (int i = distance; i > 0; i--) 
+		{
+			matrix[row][col] = count;
+			count++;
+
+			switch (dir)
+			{
+			case 1: // 오른쪽 이동
+				if (i > 1)
+					col++;
+				else
+				{
+					row++;
+					dir++;
+					distance--;
+				}
+				break;
+			case 2: // 아래쪽 이동
+				if (i > 1)
+					row++;
+				else
+				{
+					col--;
+					dir++;
+				}
+				break;
+			case 3: // 왼쪽 이동
+				if (i > 1)
+					col--;
+				else
+				{
+					row--;
+					dir++;
+					distance--;
+				}
+				break;
+			case 4: // 위쪽 이동
+				if (i > 1)
+					row--;
+				else
+				{
+					col++;
+					dir = 1;
+				}
+				break;
+			default:
+				;
+			}
+		}
+	}
+
+	// 출력
+	printf("결과\n");
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			printf("%3d", matrix[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
 
 //int main()
 //{
@@ -251,11 +323,9 @@
 //
 //int main()
 //{
-//	int matrix[MAX_ROW][MAX_COL];
-//	int* pmatrix[MAX_ROW] = { 0 };
+//	int matrix[MAX_ROW][MAX_COL] = { 0 };
+//	//int* pmatrix[MAX_ROW] = { 0 };
 //	int num = 1;
-//	int sumX = 0;
-//	int sumY = 0;
 //
 //	// 초기화 하면서 가로 방향 합산
 //	for (int i = 0; i < MAX_ROW - 1; i++)
@@ -263,11 +333,8 @@
 //		for (int j = 0; j < MAX_COL - 1; j++)
 //		{
 //			matrix[i][j] = num++;
-//			sumX += matrix[i][j];
+//			matrix[i][MAX_COL - 1] += matrix[i][j];
 //		}
-//		
-//		matrix[i][MAX_COL - 1] = sumX;
-//		sumX = 0;
 //	}
 //
 //	// 세로 방향 합산
@@ -275,11 +342,8 @@
 //	{
 //		for (int j = 0; j < MAX_ROW - 1; j++)
 //		{
-//			sumY += matrix[j][i];
+//			matrix[MAX_ROW - 1][i] += matrix[j][i];
 //		}
-//
-//		matrix[MAX_ROW - 1][i] = sumY;
-//		sumY = 0;
 //	}
 //
 //	// 출력
@@ -297,73 +361,85 @@
 
 #pragma region 지그재그 출력
 
-#define MAX_ROWCOL 4
-
-int main()
-{
-	int matrix[MAX_ROWCOL][MAX_ROWCOL] = { 0 };
-	int dir = 1; // 1일때 우상, 0일때 좌하
-	int currentNum = 1; // 현재 숫자
-	int i = 0, j = 0; // i - 행, j - 열
-
-	printf("N의 크기 : %d\n", MAX_ROWCOL);
-
-	while (currentNum <= MAX_ROWCOL * MAX_ROWCOL)
-	{
-		matrix[i][j] = currentNum;
-		currentNum++;
-
-		if (dir) // 오른쪽 위 대각선 이동
-		{
-			if (j + 1 <= MAX_ROWCOL)
-			{
-				j++;
-				dir = 0;
-			}
-			else if (i != 0 && j != MAX_ROWCOL - 1)
-			{
-				j++;
-				i--;
-			}
-			else
-			{
-				i++;
-				dir = 0;
-			}
-		}
-		else // 왼쪽 아래 대각선 이동
-		{
-			if (i + 1 <= MAX_ROWCOL)
-			{
-				i++;
-				dir = 0;
-			}
-			else if (j != 0 && i != MAX_ROWCOL - 1)
-			{
-				i++;
-				j--;
-			}
-			else
-			{
-				j++;
-				dir = 0;
-			}
-		}
-	}
-
-	// 출력
-	for (int i = 0; i < MAX_ROWCOL; i++)
-	{
-		for (int j = 0; j < MAX_ROWCOL; j++)
-		{
-			printf("%3d", matrix[i][j]);
-		}
-		printf("\n");
-	}
-
-
-
-	return 0;
-}
+//#define MAX_ROWCOL 5
+//
+//int get_abs(int x, int y);
+//
+//int main()
+//{
+//	int matrix[MAX_ROWCOL][MAX_ROWCOL] = { 0 };
+//	int edgeDir = 0; // 모서리 진행 방향 : 0-오른쪽, 1-아래쪽
+//	int diagonalDir = 0; // 대각선 진행 방향 : 0-우상, 1-좌하
+//	int distance = 0; // 대각선 이동 거리 : 방향전환 할때 (row-col) 절대값
+//	int row = 0, col = 0; // row-행, col-열
+//	int count = 1;
+//
+//	printf("N의 크기 : %d\n", MAX_ROWCOL);
+//	while (count < MAX_ROWCOL * MAX_ROWCOL + 1)
+//	{
+//		matrix[row][col] = count;
+//		count++;
+//
+//		if (distance > 0) // 대각선 이동
+//		{
+//			if (diagonalDir) // 좌하 방향 이동
+//			{
+//				row++;
+//				col--;
+//				distance--;
+//
+//				if (row == MAX_ROWCOL - 1) // 아래쪽 끝 도달 시 오른쪽 이동
+//					edgeDir = 0;
+//				else // 아닐 시 아래쪽 이동
+//					edgeDir = 1;
+//			}
+//			else // 우상 방향 이동
+//			{
+//				row--;
+//				col++;
+//				distance--;
+//
+//				if (col == MAX_ROWCOL - 1) // 오른쪽 끝 도달 시 아래쪽 이동
+//					edgeDir = 1;
+//				else // 아닐 시 오른쪽 이동
+//					edgeDir = 0;
+//			}
+//		}
+//		else // 모서리 이동
+//		{
+//			if (edgeDir) // 아래쪽 이동
+//			{
+//				row++;
+//				distance = get_abs(row, col); // 대각선 이동 거리는 |row - col|
+//				diagonalDir ^= 1; // 대각선 뱡향은 반대로 바뀜 (diagonalDir XOR 1)
+//			}
+//			else // 오른쪽 이동
+//			{
+//				col++;
+//				distance = get_abs(row, col); // 대각선 이동 거리는 |row - col|
+//				diagonalDir ^= 1; // 대각선 뱡향은 반대로 바뀜 (diagonalDir XOR 1)
+//			}
+//		}
+//	}
+//
+//	// 출력
+//	for (int i = 0; i < MAX_ROWCOL; i++)
+//	{
+//		for (int j = 0; j < MAX_ROWCOL; j++)
+//		{
+//			printf("%3d", matrix[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+//
+//// 절대값 구하기
+//int get_abs(int x, int y) 
+//{
+//	if (x - y > 0) return x - y;
+//	else return -(x - y);
+//}
 
 #pragma endregion
